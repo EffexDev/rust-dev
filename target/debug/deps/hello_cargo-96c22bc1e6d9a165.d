161f715499cbba30d468922a875909c378757f9f
EffexDev <EffexDev@gmail.com>
2024-08-11 12:42:10 +0800

    Test

---
+++ target/debug/deps/hello_cargo-96c22bc1e6d9a165.d
@@ -0,0 +1,5 @@
+/home/effex-dev/Documents/learn-rust/target/debug/deps/hello_cargo-96c22bc1e6d9a165: src/main.rs
+
+/home/effex-dev/Documents/learn-rust/target/debug/deps/hello_cargo-96c22bc1e6d9a165.d: src/main.rs
+
+src/main.rs:
