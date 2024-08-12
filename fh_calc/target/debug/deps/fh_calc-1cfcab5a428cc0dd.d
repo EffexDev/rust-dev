2602fa273987d435d1e410b42f6db7bd3e06aed9
EffexDev <EffexDev@gmail.com>
2024-08-12 15:44:24 +0800

    removed targets

--- fh_calc/target/debug/deps/fh_calc-1cfcab5a428cc0dd.d
+++
@@ -1,5 +0,0 @@
-/home/effex-dev/Documents/learn-rust/fh_calc/target/debug/deps/fh_calc-1cfcab5a428cc0dd: src/main.rs
-
-/home/effex-dev/Documents/learn-rust/fh_calc/target/debug/deps/fh_calc-1cfcab5a428cc0dd.d: src/main.rs
-
-src/main.rs:
--- fh_calc/target/debug/fh_calc.d
+++
@@ -1,1 +0,0 @@
-/home/effex-dev/Documents/learn-rust/fh_calc/target/debug/fh_calc: /home/effex-dev/Documents/learn-rust/fh_calc/src/main.rs
