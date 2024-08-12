5e6fd52c35c111d1e1dd5e665bbbbacdc5212db3
EffexDev <EffexDev@gmail.com>
2024-08-12 15:36:28 +0800

    moved folder

---
+++ Pro-Rata calculator/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/effex-dev/Documents/learn-rust/target/debug/hello_cargo: /home/effex-dev/Documents/learn-rust/src/main.rs
---
+++ Pro-Rata calculator/target/release/deps/hello_cargo-2bc0df2907fe8f78.d
@@ -0,0 +1,5 @@
+/home/effex-dev/Documents/learn-rust/target/release/deps/hello_cargo-2bc0df2907fe8f78: src/main.rs
+
+/home/effex-dev/Documents/learn-rust/target/release/deps/hello_cargo-2bc0df2907fe8f78.d: src/main.rs
+
+src/main.rs:
