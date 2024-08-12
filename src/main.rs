f9b38ec1985c2bb45d1bbe825abac2937b24db6d
EffexDev <EffexDev@gmail.com>
2024-08-12 14:45:41 +0800

    Testing input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    let x: u32 = 4;
-        println!("This is a test variable {}", x)
+    use std::io;
+
+    let mut amount_owing = String::new();
+
+    println!("Please enter the current amount owing:");
+    io::stdin().read_line(&mut amount_owing).expect("Error");
+    let amount_owing:f32 = amount_owing.trim().parse().unwrap();
+    println!("{}", amount_owing)
 }
