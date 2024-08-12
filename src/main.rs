8883fb39ed3112eabb438f4b68305ac5337e917a
EffexDev <EffexDev@gmail.com>
2024-08-12 15:34:00 +0800

    Prorata calc

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
     use std::io;
 
-    let mut amount_owing = String::new();
+    let mut monthly_cost = String::new();
 
-    println!("Please enter the current amount owing:");
-    io::stdin().read_line(&mut amount_owing).expect("Error");
-    let amount_owing:f32 = amount_owing.trim().parse().unwrap();
-    println!("{}", amount_owing)
+    println!("Please enter the monthly cost:");
+    io::stdin().read_line(&mut monthly_cost).expect("Error");
+    let monthly_cost:f32 = monthly_cost.trim().parse().unwrap();
+
+    let mut credit_time = String::new();
+
+    println!("How many days are we crediting for?");
+    io::stdin().read_line(&mut credit_time).expect("Error");
+    let credit_time:f32 = credit_time.trim().parse().unwrap();
+
+    let amount_used = (monthly_cost / 30.00) * credit_time;
+    let comp_amount:f32 = monthly_cost - amount_used;
+    println!("{}", comp_amount)
 }
