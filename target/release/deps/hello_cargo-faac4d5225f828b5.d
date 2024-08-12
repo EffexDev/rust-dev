42e9fbe1587477d9bc24b2cceb0c9ebda1d3cf31
EffexDev <EffexDev@gmail.com>
2024-08-12 14:51:27 +0800

    removed target folder

--- target/release/deps/hello_cargo-faac4d5225f828b5.d
+++
@@ -1,5 +0,0 @@
-/home/effex-dev/Documents/learn-rust/target/release/deps/hello_cargo-faac4d5225f828b5: src/main.rs
-
-/home/effex-dev/Documents/learn-rust/target/release/deps/hello_cargo-faac4d5225f828b5.d: src/main.rs
-
-src/main.rs:
